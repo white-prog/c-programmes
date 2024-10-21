9c57d0c357b8f2f63e82774cb74ee0a4756b6eaa
white-prog <sabinhameed55@gmail.com>
2024-10-21 19:21:56 +0530

    selection sort updated

--- sorting/selection_sort.c
+++ sorting/selection_sort.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    int numbers[7] = {5,1,3,2,4,6,9};
+    int numbers[7] = {5,1,3,8,4,6,9};
     int length = sizeof(numbers)/sizeof(numbers[0]);
-    int min = numbers[0];
-    int pos = 0;
-    for (int i = 0; i < length; i++)
-    {
-        for (int j = i; j < length; j++)
+    for (int i = 0; i < length; i++){
+
+        for (int j = i+1; j < length; j++)
         {
-            if(numbers[j] < min){
-                min  = numbers[j];
-                pos = j;
+            if(numbers[j] < numbers[i]){
+                int min = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = min;
 
             }
-            numbers[pos] = numbers[i];
-            numbers[i] = min;
+            
         }
        
         
     }
 
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++){
         printf("%d ",numbers[i]);
     }
-    //it's not working want to update code .......
+    //corrected it want more test cases.......
     
 
     return 0;
+
 }

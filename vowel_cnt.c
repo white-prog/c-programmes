a62e404c1d88b2960013e841a20eb1d93482adb2
white-prog <sabinhameed55@gmail.com>
2024-10-22 22:00:27 +0530

    vowel count c

---
+++ vowel_cnt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(){
+    char *letters = "how jhon eats cheese";
+    int vowel = 0;
+    for (int i = 0; i < strlen(letters); i++)
+    {
+        if(letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u'){
+            vowel += 1;
+        }
+    }
+    printf("This is %d",vowel);
+
+    return 0;
+}

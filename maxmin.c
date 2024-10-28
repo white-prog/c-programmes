5ed6fcb447d2bbf0f209c4f41b678946f4039ae6
white-prog <sabinhameed55@gmail.com>
2024-10-28 18:20:55 +0530

    maxmin.c

--- maxmin.c
+++ maxmin.c
@@ -1,8 +1,22 @@
-#include <stdio.h>>
+#include <stdio.h>
 
 int main(){
     int num1,num2,num3;
+    num1 = 28;
+    num2 = 89;
+    num3 = 45;
+    if (num1 > num2 && num1 > num3)
+    {
+        printf("%d is greater",num1);
+    }else if (num2 > num1 && num2 > num3)
+    {
+        printf("%d is greater",num2);
+    }else{
+        printf("%d is greater",num3);
+    }
     
+    
+
 
 
 

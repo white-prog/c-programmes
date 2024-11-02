fea560cd8f121d6757558ba69aaa9677a1c87765
white-prog <sabinhameed55@gmail.com>
2024-11-02 14:26:46 +0530

    memory management

---
+++ memory_management/address.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 50;
+    printf("0x%lx\n", &n);
+    printf("ox%p",&n);
+
+    return 0;//exit status
+}
+//& Provides the address of something stored in memory.
+ // * Instructs the compiler to go to a location in memory.

ee5ebcd705e1df5271607f2d73f66c9f1eb90321
white-prog <sabinhameed55@gmail.com>
2024-11-08 21:46:15 +0530

    Make string with pointer

--- memory_management/pointers.c
+++ memory_management/pointers.c
@@ -9,11 +9,18 @@ int main(){
     int *ptr = &num;
     printf("0x%lx\n",p);
     printf("0x%lx\n",ptr);
+    printf("%d\n",*p);
     char s[] = "HI!";
+    printf("%p\n",&s);
     printf("0x%lx\n", &s[0]);
     printf("0x%lx\n", &s[1]);
     printf("0x%lx\n", &s[2]);
     printf("0x%lx\n", &s[3]);
+    printf("%i\n",p);
+    //using pointer to create a string
+
+    char *name = "Messi";
+    printf("%s\n",name);
 
 
 

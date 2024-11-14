fcc1ec8db5aeb063abcd472ee013ea98171662f0
white-prog <sabinhameed55@gmail.com>
2024-11-14 17:55:38 +0530

    swap.c modified

--- memory_management/swap.c
+++ memory_management/swap.c
@@ -8,9 +8,17 @@ int main(){
 
     int x = 2;
     int y = 4;
+    int *p = &x;
     printf("%i and %i before",x,y);
+    printf("\n0x%lx and 0x%lx",&x,&y);
+    printf("--------");
     swap(&x,&y);
-    printf("%i and %i after",x,y);
+    printf("\n%i and %i after",x,y);
+    printf("\n0x%lx and 0x%lx",&x,&y);
+    printf("\n%d",*p);//*p checks for value at that memory address
+    
+    
+
 
     //swaping values in variables used pointers to swap it because x and y have scope in main function only
     //another use of memory address is in scanf using scanf("%data type",&address of variable) to store

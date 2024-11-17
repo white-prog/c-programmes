4ce7c9205868506948be77e3a4d3f7d816672985
white-prog <sabinhameed55@gmail.com>
2024-11-17 11:01:42 +0530

    debugging not completed & data structure also

---
+++ data_structure/stack_players.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//in future coding will become writing with help of NLP
+//just created a stack of player_numbers want to create functions pop push and show and also want to code things like stack overflow :) 
+typedef struct{
+    int player_no[11];
+    int size;
+}stack;
+int main(){
+
+
+
+    return 0;
+}
---
+++ practice_debug_in_c/missingnumber.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int findMissingNumber(int arr[], int size); // Function to find the missing number
+
+int main() {
+    int n;
+
+    printf("Enter the size of the array (n): ");
+    scanf("%d", &n); // Read the size of the array
+
+    int arr[n];
+
+    printf("Enter %d numbers (1 to %d, with one missing):\n", n, n + 1);
+    for (int i = 0; i < n; i++) { // Loop to read the array elements
+        scanf("%d", &arr[i]);
+    }
+
+    int missing = findMissingNumber(arr, n);
+    printf("The missing number is: %d\n", missing);
+
+    return 0;
+}
+
+int findMissingNumber(int arr[], int size) {
+    int totalSum = (size + 1) * (size + 2) / 2; // Calculate the total sum of 1 to n+1
+    int actualSum = 0;
+
+    for (int i = 0; i <= size; i++) { // Loop to calculate the actual sum of array elements
+        actualSum += arr[i];
+    }
+
+    return actualSum - totalSum; // Return the missing number
+}

0111d4b99af7d03588683bf3ce78f6c33c31d185
white-prog <sabinhameed55@gmail.com>
2024-11-28 22:24:46 +0530

    data structure not completed yet

--- data_structure/stack_players.c
+++ data_structure/stack_players.c
@@ -2,12 +2,16 @@
 //in future coding will become writing with help of NLP
 //just created a stack of player_numbers want to create functions pop push and show and also want to code things like stack overflow :) 
 typedef struct{
-    int player_no[11];
+    int player_no;
     int size;
 }stack;
 int main(){
-
-
+    stack stk[10];
+    stk->player_no = 10;
+    stk->size = 9;
+    stk->player_ = 9;
+    printf("%d",stk->player_no);
+    //not proper
 
     return 0;
 }

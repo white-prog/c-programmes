35d7b4d86531d7064831cf37cc1c3a70ec2eaedd
white-prog <sabinhameed55@gmail.com>
2024-12-03 17:25:29 +0530

    data_structure

--- data_structure/stack.c
+++ data_structure/stack.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 //in future coding will become writing with help of NLP
 //just created a stack of player_numbers want to create functions pop push and show and also want to code things like stack overflow :) 
-typedef struct{
-    int player_no;
+typedef struct
+{
+    int value[capacity];
     int size;
 }stack;
-int main(){
-    stack stk[10];
-    stk->player_no = 10;
-    stk->size = 9;
-    stk->player_ = 9;
-    printf("%d",stk->player_no);
-    //not proper
 
-    return 0;
+
+
+
+int main(){
+
+    
 }

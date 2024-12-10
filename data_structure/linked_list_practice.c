a8ed5dd0472ab97916596b483d6c25bbba81a2c0
white-prog <sabinhameed55@gmail.com>
2024-12-10 11:45:39 +0530

    Linked list practice

---
+++ data_structure/linked_list_practice.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+    int number;
+    struct node *next;
+
+}node;
+
+int main(){
+    node *list = NULL;
+    node *n = malloc(sizeof(node));
+    n -> number = 12;
+    n -> next = NULL;
+    list = n;
+    node *m = malloc(sizeof(node));
+    m -> number = 20;
+    m -> next = list;
+    list = m;
+    while (list != NULL)
+    {
+        printf("%i\n", list->number);
+        list = list->next;
+    }
+
+
+    return 0;
+}
